872116b48858eaab2bf14bc7a96376acf56fbd7e
Vivek Singhwal <vivek.singhwal@pyther.com>
2021-04-14 16:50:42 +0530

    [add plugin ]

--- platforms/android/app/src/main/assets/www/cordova_plugins.js
+++
@@ -1,8 +0,0 @@
-cordova.define('cordova/plugin_list', function(require, exports, module) {
-  module.exports = [];
-  module.exports.metadata = {
-    "cordova-plugin-whitelist": "1.3.4",
-    "cordova-plugin-androidx-adapter": "1.1.3",
-    "cordova-plugin-console": "1.1.0"
-  };
-});
--- platforms/ios/platform_www/cordova_plugins.js
+++ platforms/ios/platform_www/cordova_plugins.js
@@ -1,8 +1,18 @@
 cordova.define('cordova/plugin_list', function(require, exports, module) {
-  module.exports = [];
+  module.exports = [
+    {
+      "id": "cordova-plugin-firebasex.FirebasePlugin",
+      "file": "plugins/cordova-plugin-firebasex/www/firebase.js",
+      "pluginId": "cordova-plugin-firebasex",
+      "clobbers": [
+        "FirebasePlugin"
+      ]
+    }
+  ];
   module.exports.metadata = {
     "cordova-plugin-androidx": "3.0.0",
     "cordova-plugin-androidx-adapter": "1.1.3",
-    "cordova-plugin-whitelist": "1.3.4"
+    "cordova-plugin-whitelist": "1.3.4",
+    "cordova-plugin-firebasex": "12.1.0"
   };
 });
